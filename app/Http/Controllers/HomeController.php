376100b84cc7941b33bcb97582357e221a94854a
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-09-27 17:26:32 +0200

    Database seeded

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Models\Train;
+use App\Models\Train;
 
 class HomeController extends Controller
 {
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for($i = 0; $i < 10; $i++){
+            $train = new Train();
+
+            $train->azienda = $faker->word();
+            $train->stazione_di_partenza = $faker->word();
+            $train->stazione_di_arrivo = $faker->word();
+            $train->orario_di_partenza = $faker->time();
+            $train->orario_di_arrivo = $faker->time();
+            $train->codice_treno = $faker->randomNumber(7, true);
+            $train->numero_carrozze = $faker->randomNumber(2, true);
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+
+            $train->save();
+        }
+    }
+}
