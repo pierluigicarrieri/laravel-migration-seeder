7a0eeec2dc42f1faefa04e38f8166dd8d7609ae7
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-09-27 16:14:13 +0200

    Controller linked, Model linked to controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Models\Train;
 
 class HomeController extends Controller
 {
-    
+    public function index() {
+		return view("home");
+	}
 }
