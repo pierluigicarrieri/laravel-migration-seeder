eeab45fe48e18d1008c59b71ea77f4f06487cc01
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-09-27 16:40:53 +0200

    Set first migration to create trains table

---
+++ database/migrations/2023_09_27_143906_create_trains.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->string('orario_di_partenza', 10);
+            $table->string('orario_di_arrivo', 10);
+            $table->string('codice_treno', 20);
+            $table->tinyInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
+
